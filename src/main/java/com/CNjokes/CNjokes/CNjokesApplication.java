b33b81478a4dccb849542e10d574a24d15b382ef
Kamilalk <03KLKrukowska@gmail.com>
2022-06-01 13:30:24 +0200

    Service layer & Controllers

--- src/main/java/com/CNjokes/CNjokes/CNjokesApplication.java
+++ src/main/java/com/CNjokes/CNjokes/CNjokesApplication.java
@@ -1,5 +1,6 @@
 package com.CNjokes.CNjokes;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/CNjokes/CNjokes/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.CNjokes.CNjokes.services;
+
+public interface JokeService {
+    String getJoke();
+}
