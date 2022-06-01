b9490f61dbde09f0a822285083bdc84b678c4c74
Kamilalk <03KLKrukowska@gmail.com>
2022-06-01 11:45:04 +0200

    initial project folders

---
+++ src/main/java/com/CNjokes/CNjokes/CNjokesApplication.java
@@ -0,0 +1,13 @@
+package com.CNjokes.CNjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CNjokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CNjokesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CNjokes/CNjokes/CNjokesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CNjokes.CNjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CNjokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
